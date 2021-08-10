d2002b645191b2267949d4ec1c25d89f76c06f47
Richard <digitalsadhu@gmail.com>
2021-08-10 14:21:17 +0200

    build: set correct base url

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,9 +1,10 @@
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
 
 /**
  * @type {import('vite').UserConfig}
  */
 export default {
+  base: "/fabric-vue/",
   plugins: [vue()],
-  server: { host: '0.0.0.0' }
-}
+  server: { host: "0.0.0.0" },
+};
