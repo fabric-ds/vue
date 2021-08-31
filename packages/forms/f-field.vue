a05a8ad8caba2f2510e8ecc63d35424db7fbe424
Benjamin Akar <benjaminakar2001@gmail.com>
2021-08-31 11:45:26 +0200

    Change container to fieldset

--- packages/forms/f-field.vue
+++ packages/forms/f-field.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="field" :class="{ 'is-invalid': hasErrorMessage, 'is-disabled': disabled, [$attrs.class || '']: true }" :role="role" v-bind="aria">
+  <fieldset class="field" :class="{ 'is-invalid': hasErrorMessage, 'is-disabled': disabled, [$attrs.class || '']: true }" :role="role" v-bind="aria">
     <label v-if="label" class="field-label" :id="labelId" :for="id">{{ label }}<span v-if="optional" class="pl-8 font-normal text-14 text-gray-500"> (valgfritt)</span></label>
     <slot :triggerValidation="triggerValidation" :for="id" :labelId="labelId" />
     <slot name="control" :form="collector" />
@@ -8,7 +8,7 @@
       <span v-if="hint && hasErrorMessage">, </span>
       <span :id="errorId" v-if="hasErrorMessage">{{ validationMsg }}</span>
     </div>
-  </div>
+  </fieldset>
 </template>
 
 <script>
@@ -40,6 +40,7 @@ export default {
     disabled: Boolean,
   },
   setup(props, { attrs, slots }) {
+
     const { triggerValidation, valid, validationMsg, hasErrorMessage, collector } = createValidation(props)
 
     const labelId = computed(() => (props.label || slots.label) && (props.id + ':label'))
