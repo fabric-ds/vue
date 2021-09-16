38358032bd4b74b4b91b816d973c75c6e6c18414
Dave Honneffer <pearofducks@gmail.com>
2021-09-16 09:35:07 +0200

    Improve a11y of breadcrumbs

--- packages/breadcrumbs/entry.js
+++ packages/breadcrumbs/entry.js
@@ -1,5 +1,5 @@
 import { installer } from '@finn-no/fabric-vue-utilities'
-import fBreadcrumbs from './f-breadcrumbs.js'
+import fBreadcrumbs from './f-breadcrumbs.vue'
 
 export default {
   install: installer([fBreadcrumbs])
