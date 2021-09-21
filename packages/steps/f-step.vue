4c8c43f3f955906aa1deb479778672455946fbed
tor-martin-storsletten <tor-martin.storsletten@finn.no>
2021-09-21 08:29:55 +0200

    fix(steps): Expose complete state to screen readers
    ... And move aria-current so it only covers the SVG, so the current info is not read aloud multiple times.

--- packages/steps/f-step.vue
+++ packages/steps/f-step.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :aria-current="active ? 'step' : undefined" class="f-step" :class="{
+  <div class="f-step" :class="{
       [c.stepVertical]: vertical,
       [c.stepVerticalLeft]: vertical && left,
       [c.stepVerticalRight]: vertical && !left,
@@ -11,7 +11,7 @@
       [c.stepLineIncomplete]: !active && !complete,
       [c.stepLineComplete]: active || complete
     }" />
-    <div :class="{
+    <div :aria-current="active ? 'step' : undefined" :class="{
       [c.stepDot]: true,
       [c.stepDotVertical]: vertical,
       [c.stepDotVerticalLeft]: vertical && left,
@@ -21,7 +21,7 @@
       [c.stepDotActive]: active,
       [c.stepDotComplete]: complete
     }">
-      <svg role="img" aria-label="✓" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="white" fill-rule="evenodd" d="M13.007 4.447a.75.75 0 01.046 1.06l-5.5 6a.75.75 0 01-1.083.023l-3-3a.75.75 0 011.06-1.06l2.446 2.446 4.971-5.423a.75.75 0 011.06-.046z" clip-rule="evenodd"></path></svg>
+      <svg role="img" :aria-label="complete ? '✓' : '⍻'" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="white" fill-rule="evenodd" d="M13.007 4.447a.75.75 0 01.046 1.06l-5.5 6a.75.75 0 01-1.083.023l-3-3a.75.75 0 011.06-1.06l2.446 2.446 4.971-5.423a.75.75 0 011.06-.046z" clip-rule="evenodd"></path></svg>
     </div>
     <div class="step-line-h-r" :class="{
       [c.stepLine]: true,
