0e6590c508a7ace85d275422a13bb83bb178ecae
Dave Honneffer <pearofducks@gmail.com>
2021-10-15 10:10:06 +0200

    Canary

--- components/breadcrumbs/index.js
+++ components/breadcrumbs/index.js
@@ -1,4 +1,9 @@
+import { defineCustomElement } from 'vue'
 import fBreadcrumbs from './f-breadcrumbs.vue'
 
 export const Breadcrumbs = { install: (app) => app.component(fBreadcrumbs.name, fBreadcrumbs) }
 export { fBreadcrumbs }
+
+if (!customElements.get('f-breadcrumbs')) {
+  customElements.define('f-breadcrumbs', defineCustomElement({ ...fBreadcrumbs, styles: ['@import "https://assets.finn.no/pkg/@fabric-ds/css/v0/fabric.min.css"'] }))
+}
--- components/button/index.js
+++ components/button/index.js
@@ -1,4 +1,9 @@
+import { defineCustomElement } from 'vue'
 import fButton from './f-button.vue'
 
 export const Button = { install: (app) => app.component(fButton.name, fButton) }
 export { fButton }
+
+if (!customElements.get('f-button')) {
+  customElements.define('f-button', defineCustomElement({ ...fButton, styles: ['@import "https://assets.finn.no/pkg/@fabric-ds/css/v0/fabric.min.css"'] }))
+}
