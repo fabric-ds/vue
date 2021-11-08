ebe88201adb16b0cadd4c808b1e6c02e833e7619
Dave Honneffer <pearofducks@gmail.com>
2021-11-08 12:31:48 +0100

    Move classes to comp-classes lib

--- components/card/f-card.vue
+++ components/card/f-card.vue
@@ -6,20 +6,8 @@
 </template>
 
 <script>
-// import { card as c } from '@fabric-ds/component-classes'
+import { card as c } from '@fabric-ds/component-classes'
 import { computed } from 'vue'
-
-const c = {
-    card: 'cursor-pointer overflow-hidden relative transition-all outline-none',
-    cardShadow: 'f-card rounded-8',
-    cardFlat: 'border rounded-4',
-    cardFlatUnselected: 'border-gray-300 hover:bg-blue-50 hover:border-blue-600 active:border-blue-700',
-    cardFlatSelected: 'border-blue-600 hover:bg-blue-100 hover:border-blue-700 active:border-blue-800',
-    cardSelected: 'bg-blue-50',
-    cardOutline: 'f-card-outline absolute rounded-8 inset-0 transition-all border-2',
-    cardOutlineUnselected: 'border-transparent',
-    cardOutlineSelected: 'border-blue-600',
-}
 
 export default {
   name: 'fCard',
