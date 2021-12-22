0c8140ab3b233985aae4d3d90103dd4b293e40a4
Dave Honneffer <pearofducks@gmail.com>
2021-12-22 14:46:01 +0100

    Fix shadow

--- components/attention/f-attention.vue
+++ components/attention/f-attention.vue
@@ -41,7 +41,7 @@ export default {
       ['border-2 relative']: true,
       ['bg-gray-700 border-gray-700 text-white rounded-4 py-6 px-8']: props.tooltip,
       ['bg-green-100 border-green-400 py-8 px-16 rounded-8']: props.callout,
-      ['bg-white border-white rounded-8 p-16 filter-shadow-20']: props.popover,
+      ['bg-white border-white rounded-8 p-16 filter drop-shadow-20']: props.popover,
     }))
 
     const targetEl = ref(null)
@@ -97,18 +97,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.filter-shadow-10 {
-  filter: drop-shadow(0px 1px 3px rgba(var(--f-bluegray-600-rgb), .12)) drop-shadow(0px 1px 3px rgba(var(--f-bluegray-600-rgb), .24));
-}
-.filter-shadow-20 {
-  filter: drop-shadow(0px 3px 6px rgba(var(--f-bluegray-600-rgb), .16)) drop-shadow(0px 3px 6px rgba(var(--f-bluegray-600-rgb), .24));
-}
-.filter-shadow-30 {
-  filter: drop-shadow(0px 10px 20px rgba(var(--f-bluegray-600-rgb), .19)) drop-shadow(0px 6px 6px rgba(var(--f-bluegray-600-rgb), .23));
-}
-.filter-shadow-40 {
-  filter: drop-shadow(0px 14px 28px rgba(var(--f-bluegray-600-rgb), .25)) drop-shadow(0px 10px 10px rgba(var(--f-bluegray-600-rgb), .22));
-}
-</style>
