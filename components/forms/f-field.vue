d02949d9ecb5f9cd9f4d2bb475e4cd8836600829
Dave Honneffer <pearofducks@gmail.com>
2022-03-23 14:12:05 +0100

    Add labelLevel prop

--- components/forms/f-field.vue
+++ components/forms/f-field.vue
@@ -1,6 +1,6 @@
 <template>
   <component :is="as" class="field" :class="{ 'is-invalid': hasErrorMessage, 'is-disabled': disabled, [$attrs.class || '']: true }" :role="role" v-bind="wrapperAria">
-    <component :is="labelType" v-if="label" class="field-label" :id="labelId" :for="labelFor">{{ label }}<span v-if="optional" class="pl-8 font-normal text-14 text-gray-500"> (valgfritt)</span></component>
+    <component :is="labelType" v-if="label" class="field-label" :id="labelId" :for="labelFor" :role="valueOrUndefined(labelLevel, 'heading')" :aria-level="valueOrUndefined(labelLevel, labelLevel)">{{ label }}<span v-if="optional" class="pl-8 font-normal text-14 text-gray-500"> (valgfritt)</span></component>
     <slot :triggerValidation="triggerValidation" :labelFor="id" :labelId="labelId" :aria="aria" />
     <slot name="control" :form="collector" />
     <div class="field-hint">
@@ -24,12 +24,15 @@ export const fieldProps = {
   invalid: Boolean,
   hint: String,
   optional: Boolean,
+  labelLevel: Number,
   rules: {
     type: Array,
     default: () => ([])
   },
   ...modelProps(),
 }
+
+const valueOrUndefined = (test, value) => test ? value : undefined
 
 export default {
   name: 'fField',
@@ -52,17 +55,17 @@ export default {
     const labelFor = computed(() => isFieldset.value ? undefined : props.id)
     const labelId = computed(() => (props.label || slots.label) && (props.id + ':label'))
     const hintId = computed(() => props.id + ':hint')
-    const errorId = computed(() => hasErrorMessage.value ? props.id + ':error' : undefined)
+    const errorId = computed(() => valueOrUndefined(hasErrorMessage.value, props.id + ':error'))
     const aria = computed(() => ({
       'aria-labelledby': labelId.value,
-      'aria-describedby': props.hint ? hintId.value : undefined,
+      'aria-describedby': valueOrUndefined(props.hint, hintId.value),
       'aria-errormessage': errorId.value,
       'aria-invalid': !valid.value || props.invalid || undefined,
       'aria-required': props.required && true
     }))
-    const wrapperAria = computed(() => isFieldset.value ? aria.value : undefined)
+    const wrapperAria = computed(() => valueOrUndefined(isFieldset.value, aria.value))
 
-    return { triggerValidation, validationMsg, hasErrorMessage, labelType, labelFor, labelId, hintId, errorId, aria, wrapperAria, collector }
+    return { triggerValidation, validationMsg, hasErrorMessage, labelType, labelFor, labelId, hintId, errorId, aria, wrapperAria, collector, valueOrUndefined }
   }
 }
 </script>
