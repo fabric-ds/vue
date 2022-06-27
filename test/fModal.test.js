7da2272730b07789535d0de223f3154c665ce8aa
Dave Honneffer <pearofducks@gmail.com>
2022-06-27 16:09:42 +0200

    Modal test

--- test/fModal.test.js
+++ test/fModal.test.js
@@ -12,45 +12,38 @@ describe('expandable', () => {
     await nextTick() // have to wait for the modal to mount/animate
     assert.equal(wrapper.text(), 'Hello Fabric')
   })
-  // it('has a title area - prop', () => {
-  //   const titleWrapper = mount(fModal, { props: { title: 'Hi' } })
-  //   assert.equal(titleWrapper.text(), 'Hi')
-
-  //   const defaultSlot = '<h1>Mom</h1>'
-  //   const fullWrapper = mount(fModal, {
-  //     props: { modelValue: true, title: 'Hi ' },
-  //     slots: { default: defaultSlot }
-  //   })
-  //   assert.equal(fullWrapper.text(), 'Hi Mom')
-  // })
-  // it('has a title area - slot', () => {
-  //   const titleSlot = '<h1>Hi </h1>'
-  //   const titleWrapper = mount(fModal, { slots: { title: titleSlot } })
-  //   assert.equal(titleWrapper.text(), 'Hi')
-
-  //   const defaultSlot = '<h1>Mom</h1>'
-  //   const fullWrapper = mount(fModal, {
-  //     props: { modelValue: true },
-  //     slots: { default: defaultSlot, title: titleSlot }
-  //   })
-  //   assert.equal(fullWrapper.text(), 'Hi Mom')
-  // })
-  // it('has a chevron', () => {
-  //   const wrapper = mount(fModal, { props: { title: 'Yay' } })
-  //   assert.include(wrapper.html(), '<svg ')
-
-  //   const noChevronWrapper = mount(fModal, { props: { title: 'Yay', chevron: false } })
-  //   assert.notInclude(noChevronWrapper.html(), '<svg ')
-  // })
-  // it('emits events', async () => {
-  //   const wrapper = mount(fModal, { props: { title: 'Yay' } })
-  //   wrapper.get('button').trigger('click')
-  //   // we have to wait twice because the event trigger itself is waiting for the DOM!
-  //   await Promise.all([nextTick, nextTick])
-  //   assert.property(wrapper.emitted(), 'expand')
-
-  //   wrapper.get('button').trigger('click')
-  //   await Promise.all([nextTick, nextTick])
-  //   assert.property(wrapper.emitted(), 'collapse')
-  // })
+  it('has a title', async () => {
+    const defaultSlot = '<h1>Hello Fabric</h1>'
+    const wrapper = mount(fModal, { props: { modelValue: true, title: 'OMG' }, slots: { default: defaultSlot } })
+    await nextTick() // have to wait for the modal to mount/animate
+    assert.include(wrapper.text(), 'OMG')
+  })
+  it('has a footer', async () => {
+    const defaultSlot = '<h1>Hello Fabric</h1>'
+    const footerSlot = '<h1>Goodbye Fabric</h1>'
+    const wrapper = mount(fModal, { props: { modelValue: true }, slots: { default: defaultSlot, footer: footerSlot } })
+    await nextTick() // have to wait for the modal to mount/animate
+    assert.include(wrapper.text(), 'Hello Fabric')
+    assert.include(wrapper.text(), 'Goodbye Fabric')
+  })
+  it('left and right are toggleable', async () => {
+    const defaultSlot = '<h1>Hello Fabric</h1>'
+    const wrapper = mount(fModal, { props: { modelValue: true }, slots: { default: defaultSlot } })
+    await nextTick()
+    const left = wrapper.find('button[aria-label="Tilbake"]').exists()
+    assert.notOk(left)
+    const right = wrapper.find('button[aria-label="Lukk"]').exists()
+    assert.notOk(right)
+  })
+  it('has sane defaults for left and right', async () => {
+    const defaultSlot = '<h1>Hello Fabric</h1>'
+    const wrapper = mount(fModal, { props: { modelValue: true, left: true, right: true }, slots: { default: defaultSlot } })
+    await nextTick()
+    const left = wrapper.find('button[aria-label="Tilbake"]')
+    assert.ok(left.exists())
+    assert.include(left.html(), '<svg ')
+    const right = wrapper.find('button[aria-label="Lukk"]')
+    assert.ok(right.exists())
+    assert.include(right.html(), '<svg ')
+  })
 })
